43ffab1758f6c97d059e2ed378fca175ad7c62a7
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-04 21:02:33 -0300

    estrutura base do projeto e tela inicial

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,27 +1,26 @@
 import React, { ReactElement } from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
+import Button from '../components/Button';
 import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
  
 export default function Welcome(): ReactElement {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
-        Gerencie suas plantas de forma fácil
+        Gerencie{'\n'}
+        suas plantas{'\n'}
+        de forma fácil
       </Text>
 
-      <Image source={wateringImg} />
+      <Image source={wateringImg} style={styles.image} />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <TouchableOpacity style={styles.button}>
-        <Text>
-          {'>'}
-        </Text>
-      </TouchableOpacity>
+      <Button title=">" />
     </SafeAreaView>
   );
 }
@@ -35,9 +34,19 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38
   },
-  subtitle: {},
-  button: {}
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    width: 292,
+    height: 284,
+  }
 });
  
