9ac6ff6670f775a6bdc9b257e18c85d3114723c6
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-05 14:18:31 -0300

    refatoração inicial

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from 'react';
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
-import Button from '../components/Button';
+import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
  
@@ -13,14 +12,22 @@ export default function Welcome(): ReactElement {
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image 
+        source={wateringImg} 
+        style={styles.image}
+        resizeMode="contain"
+      />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <Button title=">" />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>
+          {'>'}
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -29,7 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
@@ -45,8 +52,20 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
   }
 });
  
